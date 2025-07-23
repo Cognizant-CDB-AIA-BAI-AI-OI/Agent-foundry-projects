92d8025d2611429f214a6ae818492f8e1299a049
Nilavo Boral <Nilavo.Boral@cognizant.com>
2025-07-23 15:56:34 +0530

    Update main.py

--- PDF-question-answering/main.py
+++ PDF-question-answering/main.py
@@ -3,7 +3,7 @@ from utils import setup_env_and_llm                         # Initializes enviro
 from pdf_qa import pdf_text_extraction, pdf_query          # Functions for extracting text and querying PDFs
 
 
-# Inputs: model choice, PDF file path, and user query
+# ✍️ Inputs: model choice, PDF file path, and user query
 model="gemini-2.5-flash"
 pdf_path = "sample_pdfs\2072068.pdf" # Replace with your PDF file path
 user_input = "Give me 5 bullet points about this document."
