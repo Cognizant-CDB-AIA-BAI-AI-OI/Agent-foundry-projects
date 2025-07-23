c0a495889c46c9f15fcf0b377972456a13707290
Nilavo Boral <Nilavo.Boral@cognizant.com>
2025-07-24 00:48:04 +0530

    Update main.py

--- PDF-question-answering/main.py
+++ PDF-question-answering/main.py
@@ -1,12 +1,11 @@
 # 📦 Import necessary functions
 from utils import setup_env_and_llm                        # Initializes environment and language model
 from pdf_qa import pdf_text_extraction, pdf_query          # Functions for extracting text and querying PDFs
+from inputs import model, user_input                       # Inputs: model choice, and user query
 
 
-# ✍️ Inputs: model choice, PDF file path, and user query
-model="gemini-2.5-flash"
-pdf_path = "sample_pdfs\2072068.pdf" # Replace with your PDF file path
-user_input = "Give me 5 bullet points about this document."
+# ✍️ Input: PDF file path
+pdf_path = r"sample_pdfs\test.pdf" # Replace with your PDF file path
 
 
 # ⚙️ STEP 1: Set up the env & llm
